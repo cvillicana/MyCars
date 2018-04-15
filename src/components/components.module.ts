72cf607049a35ea63c71975173cc24e1cbc00674
kady Villicaña <villicana.ca@gmail.com>
2018-04-15 10:21:59 -0500

    removed component

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CarOptionsSelectComponent } from './car-options-select/car-options-select';
-@NgModule({
-	declarations: [CarOptionsSelectComponent],
-	imports: [],
-	exports: [CarOptionsSelectComponent]
-})
-export class ComponentsModule {}
